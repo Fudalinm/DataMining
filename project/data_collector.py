54aad182c96b39ac69b7bd26af9bff0d4414def4
Marek Fudaliński <fudalinm@gmail.com>
2020-03-05 17:18:36 +0100

    creating grid improve

--- project/data_collector.py
+++ project/data_collector.py
@@ -14,8 +14,7 @@ import csv
 import os
 import numpy as np
 from mpl_toolkits.basemap import Basemap
-
-# import time
+import concurrent.futures
 
 # all constances are in meters
 EQUATOR_LENGTH = int(40076000)  # this is start because we try to capture whole earth in one query
@@ -88,95 +87,114 @@ class Point:
         int_distance = int(EARTH_RADIUS * c) + 1
         return int_distance
 
-    # Something might be slightly wrong
-    @staticmethod
-    def create_earth_grid():
-        print("x")
-        bm = Basemap(resolution='c')
-        print("d")
-        grid = []
-        for i in range(0, MERIDIAN_LENGTH, 5000):
-            latitude_degree_north = (-(i / MERIDIAN_LENGTH) * 180 + 90) * (math.pi / 180)
-            latitude_degree_south = (-((i + 5000) / MERIDIAN_LENGTH) * 180 + 90) * (math.pi / 180)
-
-            longitude_degree_length_north = (math.cos(latitude_degree_north) * EQUATOR_LENGTH) / 360
-            longitude_degree_length_south = (math.cos(latitude_degree_south) * EQUATOR_LENGTH) / 360
-
-            if longitude_degree_length_north > longitude_degree_length_south:
-                longer_longitude_degree = longitude_degree_length_north
-            else:
-                longer_longitude_degree = longitude_degree_length_south
-
-            longer_longitude_perimeter = longer_longitude_degree * 360
-
-            longitude_points = []
-
-            for j in range(0, int(longer_longitude_perimeter), 5000):
-                longitude_points.append((j / longer_longitude_perimeter) * 360)
-
-            points_north = []
-            points_south = []
-            for longitude in longitude_points:
-                points_north.append(Point(latitude_degree_north, longitude))
-                points_south.append(Point(latitude_degree_south, longitude))
-
-            squares = []
-            for k in range(len(points_south)):
-                tmp = [points_north[k], points_north[(k + 1) % len(points_south)], points_south[k],
-                       points_south[(k + 1) % len(points_south)]]
-                # checking if the square is on land
-
-                if bm.is_land(tmp[0].longitude, tmp[0].latitude):
-                    squares.append(tmp)
-
-            grid.extend(squares)
-            print(latitude_degree_south)
-
-        return grid
-
-    @staticmethod
-    def save_grid(grid):
-        # grid = [ [p1,p2,p3,p4] , ....]
-        arr_to_save = np.empty(shape=(len(grid), 4, 2))
-        for square_index in range(len(grid)):
-            square = grid[square_index]
-            for i in range(len(square)):
-                arr_to_save[square_index, i, 0] = square[i].longitude
-                arr_to_save[square_index, i, 1] = square[i].latitude
-        np.save(GRID_FILE_TO_SAVE, arr_to_save)
-
-    @staticmethod
-    def load_grid():
-        arr = np.load(GRID_FILE_TO_SAVE)
-        grid = []
-
-        x, y, z = arr.shape
-        for i in range(x):
-            square = []
-            for j in range(y):
-                p = Point(arr[i, j, 1], arr[i, j, 0])
-                square.append(p)
-            grid.append(square)
-        return grid
-
     def __str__(self):
         return "##POINT##" + "\n\t#latitude: " + str(self.latitude) + "\n\t#longitude: " + str(self.longitude) + "\n"
+
+
+def create_earth_grid(start, end):
+    print("x")
+    bm = Basemap(resolution='c')
+    print("d")
+    grid = []
+    for i in range(start, end, 5000):
+        latitude_degree_north = (-(i / MERIDIAN_LENGTH) * 180 + 90) * (math.pi / 180)
+        latitude_degree_south = (-((i + 5000) / MERIDIAN_LENGTH) * 180 + 90) * (math.pi / 180)
+
+        longitude_degree_length_north = (math.cos(latitude_degree_north) * EQUATOR_LENGTH) / 360
+        longitude_degree_length_south = (math.cos(latitude_degree_south) * EQUATOR_LENGTH) / 360
+
+        if longitude_degree_length_north > longitude_degree_length_south:
+            longer_longitude_degree = longitude_degree_length_north
+        else:
+            longer_longitude_degree = longitude_degree_length_south
+
+        longer_longitude_perimeter = longer_longitude_degree * 360
+
+        longitude_points = []
+
+        for j in range(0, int(longer_longitude_perimeter), 5000):
+            longitude_points.append((j / longer_longitude_perimeter) * 360)
+
+        points_north = []
+        points_south = []
+        for longitude in longitude_points:
+            points_north.append(Point(latitude_degree_north, longitude))
+            points_south.append(Point(latitude_degree_south, longitude))
+
+        squares = []
+        for k in range(len(points_south)):
+            tmp = [points_north[k], points_north[(k + 1) % len(points_south)], points_south[k],
+                   points_south[(k + 1) % len(points_south)]]
+            # checking if the square is on land
+
+            if bm.is_land(tmp[0].longitude, tmp[0].latitude):
+                squares.append(tmp)
+
+        grid.extend(squares)
+        print(latitude_degree_south)
+
+    return grid
+
+
+def create_earth_grid_basic():
+    return create_earth_grid(0, MERIDIAN_LENGTH)
+
+
+def create_earth_grid_split():
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        step = int(MERIDIAN_LENGTH / 4)
+        g0 = executor.submit(create_earth_grid, step * 0, step * 1)
+        g1 = executor.submit(create_earth_grid, step * 1, step * 2)
+        g2 = executor.submit(create_earth_grid, step * 2, step * 3)
+        g3 = executor.submit(create_earth_grid, step * 3, step * 4)
+
+        to_ret = []
+        to_ret.extend(g0.result())
+        to_ret.extend(g1.result())
+        to_ret.extend(g2.result())
+        to_ret.extend(g3.result())
+        return to_ret
+
+
+def save_grid(grid):
+    # grid = [ [p1,p2,p3,p4] , ....]
+    arr_to_save = np.empty(shape=(len(grid), 4, 2))
+    for square_index in range(len(grid)):
+        square = grid[square_index]
+        for i in range(len(square)):
+            arr_to_save[square_index, i, 0] = square[i].longitude
+            arr_to_save[square_index, i, 1] = square[i].latitude
+    np.save(GRID_FILE_TO_SAVE, arr_to_save)
+
+
+def load_grid():
+    arr = np.load(GRID_FILE_TO_SAVE)
+    grid = []
+
+    x, y, z = arr.shape
+    for i in range(x):
+        square = []
+        for j in range(y):
+            p = Point(arr[i, j, 1], arr[i, j, 0])
+            square.append(p)
+        grid.append(square)
+    return grid
 
 
 def send_request(centre, distance):
     if centre.latitude > 90 or centre.latitude < -90 or centre.longitude > 180 or centre.longitude < -180:
         return
 
-    req_url = front_query + "distance=" + str(distance) + "&latitude=" + str(centre.latitude) + "&longitude=" \
-              + str(centre.longitude)
-
-    response = requests.get(req_url).json()
-
-    if response == []:
-        print(req_url)
-        print(response)
-
-    return response
+    to_ret = []
+    page = 1
+    while True:
+        req_url = front_query + "distance=" + str(distance) + "&latitude=" + str(centre.latitude) + "&longitude=" \
+                  + str(centre.longitude) + "&page=" + str(page)
+        response = requests.get(req_url).json()
+        page += 1
+        to_ret.extend(response)
+        if len(response) != MAX_IN_SINGLE_QUERY:
+            return to_ret
 
 
 def init_files():
@@ -190,8 +208,7 @@ def init_files():
             cw.writeheader()
 
 
-def save_to_file(file_path, data):
-    # print(data)
+def save_to_file(file_path, log_file, data, centre, distance):
     # TODO: filtering unnecesery fields
     for d in data:
         keys_to_remove = []
@@ -201,58 +218,44 @@ def save_to_file(file_path, data):
         for to_remove in keys_to_remove:
             d.pop(to_remove)
 
-    # print(data)
-
     with open(file_path, "a", newline="") as f:
         cw = csv.DictWriter(f, ELEMENTS_TO_SAVE, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         cw.writerows(data)
 
+    if len(data) == 0:
+        with open(log_file, "a+") as f:
+            f.write("No data in: latitude: {}, longitude: {}, distance: {}\n".format(centre.latitude, centre.longitude,
+                                                                                     distance))
+    else:
+        with open(log_file, "a+") as f:
+            f.write(
+                "MANY data in: latitude: {}, longitude: {}, distance: {}, data_count\n".format(centre.latitude,
+                                                                                               centre.longitude,
+                                                                                               distance, len(data)))
 
-# TODO: it should only fragment by date not position!!!
 def capture_data_fragment(points):
     centre = Point(points_list=points)
     distance = centre.distance(points[0])
     json_data = send_request(centre=centre, distance=distance)
 
-    # TODO: if distance is equal 1 we should split through time
-
-    if len(json_data) == MAX_IN_SINGLE_QUERY:
-        with open(LOG_FILE_PATH, "a+") as f:
-            f.write(
-                "MANY data in: latitude: {}, longitude: {}, distance: {}\n".format(centre.latitude, centre.longitude,
-                                                                                   distance))
-
-        save_to_file(NOT_VALID_DATA_FILE, json_data)
-        for subset in itertools.combinations(points, 2):
-            tmp_list = list(subset)
-            tmp_list.append(centre)
-            capture_data_fragment(tmp_list)
-
-    elif len(json_data) == 0:
-        # TODO: log about fragment without any records
-        with open(LOG_FILE_PATH, "a+") as f:
-            f.write("No data in: latitude: {}, longitude: {}, distance: {}\n".format(centre.latitude, centre.longitude,
-                                                                                     distance))
-    else:
-        save_to_file(PROPER_DATA_FILE, json_data)
+    save_to_file(PROPER_DATA_FILE, LOG_FILE_PATH, json_data, centre, distance)
 
 
 def create_save_grid():
-    grid = Point.create_earth_grid()
-    Point.save_grid(grid)
+    # grid = create_earth_grid_basic()
+    grid = create_earth_grid_split()
+    save_grid(grid)
     print(len(grid))
 
 
 def capture_whole_data(grid):
     init_files()
-
     for square in grid:
         capture_data_fragment(square)
 
 
 if __name__ == "__main__":
     # capture_whole_data()
-    # TODO: save grid to some csv?
-    # create_save_grid()
-    grid = Point.load_grid()
-    print(len(grid))
+    create_save_grid()
+    # grid = load_grid()
+    # print(len(grid))
